d73df98da0052c56f93b545fcffeb73b2b6d2b85
Bruno Michel <bruno@cozycloud.cc>
2026-02-23 11:58:43 +0100

    Fix markdown in project tiles

--- public/templatetags/markdown_extras.py
+++ public/templatetags/markdown_extras.py
@@ -1,7 +1,8 @@
-"""Markdown template filter."""
+"""Markdown template filters."""
 
 import markdown
 from django import template
+from django.utils.html import strip_tags
 from django.utils.safestring import mark_safe
 
 register = template.Library()
@@ -13,3 +14,12 @@ def markdown_format(text):
     if not text:
         return ""
     return mark_safe(markdown.markdown(text, extensions=["fenced_code", "tables"]))
+
+
+@register.filter(name="strip_markdown")
+def strip_markdown(text):
+    """Convert markdown to plain text by rendering and stripping HTML."""
+    if not text:
+        return ""
+    html = markdown.markdown(text, extensions=["fenced_code", "tables"])
+    return strip_tags(html)
